9c392e5567cdf5d91336bbf0e285599c0c19d075
Reddy Priya Gadikota <101251058+reddypriya-g@users.noreply.github.com>
2024-06-27 13:25:03 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from 'socket.io-client';
 
-const socket = socketIOClient('http://localhost:5000');
+const socket = socketIOClient('https://insta-api-delta.vercel.app');
 
 export default socket;
