9499763a9b81e78cc1241e94fd6b5a3c0b051a5b
Reddy Priya Gadikota <101251058+reddypriya-g@users.noreply.github.com>
2024-06-27 13:40:38 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,15 @@
-import socketIOClient from 'socket.io-client';
+// import socketIOClient from 'socket.io-client';
 
-const socket = socketIOClient('https://insta-api-delta.vercel.app');
+// const socket = socketIOClient('https://insta-api-delta.vercel.app');
+
+// export default socket;
+import { io } from 'socket.io-client';
+
+const socket = io('https://insta-api-delta.vercel.app', {
+  withCredentials: true,
+  extraHeaders: {
+    'my-custom-header': 'abcd'
+  }
+});
 
 export default socket;
