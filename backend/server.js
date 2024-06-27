800d2d2b004b65146aa79609db6b3df93c4369be
Reddy Priya Gadikota <101251058+reddypriya-g@users.noreply.github.com>
2024-06-27 13:17:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://insta-frontend-five.vercel.app/',
+    origin: 'https://insta-frontend-five.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
